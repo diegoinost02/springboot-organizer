39661ac0d5e7482600d681d02c9aea82318099ef
diegoinost02 <diegoinostroza00@gmail.com>
2024-01-29 23:10:48 -0300

    Se configuran los Cors en los controladores

--- src/main/java/com/diego/organizer/springbootorganizer/controllers/UserController.java
+++ src/main/java/com/diego/organizer/springbootorganizer/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import com.diego.organizer.springbootorganizer.services.UserService;
 import io.jsonwebtoken.JwtException;
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = {"http://localhost:4200/"}) 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
--- src/main/java/com/diego/organizer/springbootorganizer/security/SecurityConfig.java
+++ src/main/java/com/diego/organizer/springbootorganizer/security/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
         // users
         .requestMatchers(HttpMethod.POST,"/api/users/register").permitAll() // registrarse
         .requestMatchers(HttpMethod.GET,"/api/users/{id}").hasAnyRole("USER", "ADMIN") // obtener user por id
-        .requestMatchers(HttpMethod.GET,"/api/users").hasRole("ADMIN") // obtener todos los users
+        .requestMatchers(HttpMethod.GET,"/api/users").permitAll() //.hasRole("ADMIN") // obtener todos los users
         .requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN") // crear users con role de admin
         .requestMatchers(HttpMethod.POST, "/api/users/refresh").hasAnyRole("USER", "ADMIN") // refrescar token
 
@@ -71,7 +71,7 @@ public class SecurityConfig {
         .addFilter(new JwtAuthenticationFilter(this.authenticationManager())) // configuración propia
         .addFilter(new JwtValidationFilter(this.authenticationManager())) // configuración propia
         .csrf(config -> config.disable())
-        .cors(cors -> cors.configurationSource(corsConfigurationSource())) // configurar el course
+        .cors(cors -> cors.configurationSource(corsConfigurationSource())) // configurar el cors
         .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .build();
     }
@@ -79,9 +79,9 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
